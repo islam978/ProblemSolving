7c8c8e3f1c3a30148aeada63cedbe8624ecc9bfd
islam978 <90086704+islam978@users.noreply.github.com>
2023-01-17 22:32:35 +0300

    Update PrintWordsFromAAAtoZZZ.cpp

--- PrintWordsFromAAAtoZZZ.cpp
+++ PrintWordsFromAAAtoZZZ.cpp
@@ -15,8 +15,8 @@ int readPositiveNumber(string message){
 void PrintWordsFromAAAtoZZZ(){
     string word =" ";
    for(int i =65;i<=90;i++){
-       for(int j =i;j<=90;j++ ){
-           for(int k =j;k<=90;k++)
+       for(int j =65;j<=90;j++ ){
+           for(int k =65;k<=90;k++)
            {
                word = word + char(i);
                word = word + char(j);
