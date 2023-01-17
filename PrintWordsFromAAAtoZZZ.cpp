e30bfc7a7c019655c1e100d27e8032093ca08664
islam978 <90086704+islam978@users.noreply.github.com>
2023-01-17 17:46:38 +0300

    Add files via upload

---
+++ PrintWordsFromAAAtoZZZ.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+using namespace std;
+
+int readPositiveNumber(string message){
+    int num ;
+    do{
+    cout<<message<<endl;
+    cin>>num;
+    }while(num<0);
+    
+    return num;
+}
+
+void PrintWordsFromAAAtoZZZ(){
+    string word =" ";
+   for(int i =65;i<=90;i++){
+       for(int j =i;j<=90;j++ ){
+           for(int k =j;k<=90;k++)
+           {
+               word = word + char(i);
+               word = word + char(j);
+               word = word + char(k);
+               
+              cout<<word<<endl;
+              word = "";
+          
+           }
+         
+       }
+      
+   }
+     
+ }
+
+int main()
+{
+
+    PrintWordsFromAAAtoZZZ();
+    return 0;
+}
